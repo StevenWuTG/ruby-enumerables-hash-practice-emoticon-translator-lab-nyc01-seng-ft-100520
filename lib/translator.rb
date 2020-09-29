6cac57bc305ce9dd6099d75d4d0fbee06f02ec79
Steven wu <stevenwu91@icloud.com>
2020-09-29 14:15:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ def get_japanese_emoticon(path , emoticon)
   library = load_library(path)
   emoticon = library.keys.find do |key|
     library[key][:english] == emoticon
+  end
 end
  
 def get_english_meaning
