ffa1543c98eeef97552e2041c24f5a9c9f03a74b
Steven wu <stevenwu91@icloud.com>
2020-09-29 13:49:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,14 @@ require "pry"
 def load_library(path)
   # code goes here
   emoticons = YAML.load_file(path)
+  new_hash = {}
   
+  emoticons.each do |key , value|
+    new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
+  end
+  new_hash
   
 end
 
